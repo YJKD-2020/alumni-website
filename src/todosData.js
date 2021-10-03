622a57899ce0dd714e5ffabcbbc5659cc1d46bed
shagundeep <shagundeep17@gmail.com>
2021-10-03 17:05:53 +0530

    create todo page for alumni website

---
+++ src/todosData.js
@@ -0,0 +1,15 @@
+   
+const todosData = [
+    {
+        id: 1,
+        text: "Write a blog post on Building a ToDo App using React",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Build a ToDo App using React",
+        completed: true
+    }
+]
+
+export default todosData
